c7513a8fadf31d3a82daa4e6dc1f9191c6af1aae
sadekxD <sadekirfan3@gmail.com>
2022-11-06 18:14:08 +0600

    commit

--- components/sections/RoadmapSection.js
+++ components/sections/RoadmapSection.js
@@ -6,34 +6,24 @@ const RoadmapSection = () => {
 			<div className="container">
 				<div className="roadmap-left">
 					<div className="box box-1">
-						<div className="box-content">
 							<h3 className="box-heading">NFT PROJECT</h3>
 							<img src="/images/roadmap-1.png" alt="roadmap 1" />
-						</div>
 					</div>
 					<div className="box box-2">
-						<div className="box-content">
 							<h3 className="box-heading">METAVERSE</h3>
 							<img src="/images/roadmap-2.png" alt="roadmap 2" />
-						</div>
 					</div>
 					<div className="box box-3">
-						<div className="box-content">
 							<h3 className="box-heading">COMMUNITY</h3>
 							<img src="/images/roadmap-3.png" alt="roadmap 3" />
-						</div>
 					</div>
 					<div className="box box-4">
-						<div className="box-content">
 							<h3 className="box-heading">STREET</h3>
 							<img src="/images/roadmap-4.png" alt="roadmap 4" />
-						</div>
 					</div>
 					<div className="box box-5">
-						<div className="box-content">
 							<h3 className="box-heading">P2E GAME</h3>
 							<img src="/images/roadmap-5.png" alt="roadmap 5" />
-						</div>
 					</div>
 					<div className="box box-6">
 						<div className="box-content">

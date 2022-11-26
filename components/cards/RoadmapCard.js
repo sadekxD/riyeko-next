a4f2120348fd51d7dd4a0ac98b982625e8757543
sadekxD <sadekirfan3@gmail.com>
2022-11-26 22:44:58 +0600

    comimt

--- components/cards/RoadmapCard.js
+++ components/cards/RoadmapCard.js
@@ -6,10 +6,20 @@ const RoadmapCard = () => {
 	const toggle = () => setActive(!active);
 	const boxRef = useRef();
 	const [dim, setDim] = useState({ x: 0, y: 0 });
+	const [innerWidth, setInnerWidth] = useState();
 
 	useEffect(() => {
 		setDim({ x: boxRef.current.clientWidth, y: boxRef.current.clientHeight });
-	}, [active]);
+	}, [active, innerWidth]);
+
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			setInnerWidth(window.innerWidth);
+			window.addEventListener("resize", () => {
+				setInnerWidth(window.innerWidth);
+			});
+		}
+	}, [innerWidth]);
 
 	const ref = useDetectClickOutside({
 		onTriggered: () => {
@@ -17,61 +27,112 @@ const RoadmapCard = () => {
 		},
 	});
 
-	console.log(dim);
+	console.log(innerWidth);
 
-	return (
-		<div
-			className="roadmap-card"
-			onClick={toggle}
-			ref={ref}
-			style={{ height: dim.y }}
-		>
+	if (innerWidth <= 600) {
+		return (
 			<div
-				className="card-inner"
-				style={{
-					transform: active ? "rotateY(180deg)" : "rotateY(0deg)",
-				}}
+				className="roadmap-card"
+				onClick={toggle}
+				ref={ref}
+				style={{ height: dim.y }}
 			>
-				<div className="card-front" data-title="NFT PROJECT">
-					<img src="/images/roadmap-1.png" alt="roadmap 1" />
-				</div>
 				<div
-					className="card-back"
+					className="card-inner"
 					style={{
-						height: !active ? "210px" : "fit-content",
+						transform: active ? "rotateY(180deg)" : "rotateY(0deg)",
 					}}
-					ref={boxRef}
 				>
-					<p>
-						Riyeko is a collection of 5,555 players chillin’ on the Ethereum
-						blockchain.
-					</p>
-					<br />
-					<p>
-						NFTs are here to rule the new era of the corporate world. From small
-						business owners to large
-					</p>
-					<br />
-					<p>
-						corporations and Tech Giants, everyone is eager to venture into the
-						NFT ecosystem.
-					</p>
-					<br />
-					<p>
-						But… the distinct detachment from the digital world to the physical
-						world is NFT’s biggest predicament. We strive to be the bridge
-						between both worlds.
-					</p>
-					<br />
-					<p>
-						Our goal is to also be the biggest and most innovative community in
-						the Web3.0 eco-system that focuses on building together.
-					</p>
-					<p>Stay tuned for more announcements on this journey!</p>
+					<div className="card-front" data-title="NFT PROJECT">
+						<img src="/images/roadmap-1.png" alt="roadmap 1" />
+					</div>
+					<div
+						className="card-back"
+						style={{
+							height: !active ? "210px" : "fit-content",
+						}}
+						ref={boxRef}
+					>
+						<p>
+							Riyeko is a collection of 5,555 players chillin’ on the Ethereum
+							blockchain.
+						</p>
+						<br />
+						<p>
+							NFTs are here to rule the new era of the corporate world. From
+							small business owners to large
+						</p>
+						<br />
+						<p>
+							corporations and Tech Giants, everyone is eager to venture into
+							the NFT ecosystem.
+						</p>
+						<br />
+						<p>
+							But… the distinct detachment from the digital world to the
+							physical world is NFT’s biggest predicament. We strive to be the
+							bridge between both worlds.
+						</p>
+						<br />
+						<p>
+							Our goal is to also be the biggest and most innovative community
+							in the Web3.0 eco-system that focuses on building together.
+						</p>
+						<p>Stay tuned for more announcements on this journey!</p>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	} else {
+		return (
+			<div className="roadmap-card" onClick={toggle} ref={ref}>
+				<div
+					className="card-inner"
+					style={{
+						transform: active ? "rotateY(180deg)" : "rotateY(0deg)",
+					}}
+				>
+					<div className="card-front" data-title="NFT PROJECT">
+						<img src="/images/roadmap-1.png" alt="roadmap 1" />
+					</div>
+					<div
+						className="card-back"
+						style={{
+							height: !active ? "210px" : "fit-content",
+						}}
+						ref={boxRef}
+					>
+						<p>
+							Riyeko is a collection of 5,555 players chillin’ on the Ethereum
+							blockchain.
+						</p>
+						<br />
+						<p>
+							NFTs are here to rule the new era of the corporate world. From
+							small business owners to large
+						</p>
+						<br />
+						<p>
+							corporations and Tech Giants, everyone is eager to venture into
+							the NFT ecosystem.
+						</p>
+						<br />
+						<p>
+							But… the distinct detachment from the digital world to the
+							physical world is NFT’s biggest predicament. We strive to be the
+							bridge between both worlds.
+						</p>
+						<br />
+						<p>
+							Our goal is to also be the biggest and most innovative community
+							in the Web3.0 eco-system that focuses on building together.
+						</p>
+						<p>Stay tuned for more announcements on this journey!</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
 };
 
 export default RoadmapCard;
